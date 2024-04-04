4e2519c9a60b298f22f3ed337bbdbbc1b5de7bcb
FabioSimoDev <fabiofabietto182@gmail.com>
2024-04-04 14:03:35 +0200

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ToastProvider from "./context/ToastContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <ToastProvider>
       <App />
     </ToastProvider>
