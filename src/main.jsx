3eb40524f9e5338e3d66d8aef3ec1288808f3442
FabioSimoDev <fabiofabietto182@gmail.com>
2024-04-04 14:04:35 +0200

    vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import ToastProvider from "./context/ToastContext.jsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <ToastProvider>
       <App />
     </ToastProvider>
